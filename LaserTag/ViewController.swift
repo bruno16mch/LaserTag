366f31c3ef99a4bef18f44e72672c4414b5ab25c
Bruno Rangel Huerta <bruno14.rangel@gmail.com>
2017-03-31 15:13:07 -0500

    Initial Commit

---
+++ LaserTag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaserTag
+//
+//  Created by Bruno Rangel on 3/31/17.
+//  Copyright © 2017 Bruno Rangel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
